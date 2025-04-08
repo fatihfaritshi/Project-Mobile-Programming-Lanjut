ae7b5e534c62827ce812f863976950a5f792c098
fatihfaritshi <fatihxfaritsi@gmail.com>
2025-04-08 21:26:26 +0700

    JS 5: Navigasi

--- belajar_navigasi/lib/main.dart
+++ belajar_navigasi/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Navigation Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 6, 3, 100)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 34, 28, 188)),
       ),
       initialRoute: '/',
       routes: {
